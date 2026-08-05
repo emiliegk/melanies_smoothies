5ac3fb640011935bca5a7bed0343b63caeeb5ce3
Emilie Grønborg Kristensen <144821149+emiliegk@users.noreply.github.com>
2026-08-05 11:11:02 +0200

    Integrate Smoothiefroot API for smoothie orders

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,14 +41,16 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-    # st.write(ingredients_string)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
+
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+   
 
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                         values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
     
-    # st.write(my_insert_stmt)
-    # st.stop()
+   
 
     time_to_insert = st.button('Submit Order'
     )
@@ -56,6 +58,4 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
     
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")  
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
